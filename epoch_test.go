aca984fb89027e85b3ab49504496a4acdfbcddd0
Arne Wiese <arne.wiese@gmail.com>
2017-06-20 15:08:55 +0200

    Made test nice-looking

--- epoch_test.go
+++ epoch_test.go
@@ -5,12 +5,13 @@ import (
 )
 
 func TestWithCorrectTimestamp(t *testing.T) {
-	expected := "2017-06-10 00:00:00 +0000 UTC"
-
 	ts := Timestamp{}
 	ts.FromString("1497052800")
 
-	if ts.UtcString() != expected {
-		t.Error("expected", expected, "got", ts.UtcString())
+	expected := "2017-06-10 00:00:00 +0000 UTC"
+	got := ts.UtcString()
+
+	if got != expected {
+		t.Error("expected", expected, "got", got)
 	}
 }

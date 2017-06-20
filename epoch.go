89a2ee0699db245c430897d40fea08f94f9bb087
Arne Wiese <arne.wiese@gmail.com>
2017-06-20 11:29:57 +0200

    intial commit

---
+++ epoch.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"time"
+	"strconv"
+)
+
+type Timestamp struct {
+	time.Time
+}
+
+func (t *Timestamp) FromString(stamp string) error {
+	ts, err := strconv.ParseInt(stamp, 10, 64)
+
+	if err != nil {
+		return err
+	}
+
+	t.Time = time.Unix(ts, 0)
+
+	return nil
+}
+
+func main() {
+	for _, timeAsString := range os.Args[1:] {
+		ts := Timestamp{}
+		ts.FromString(timeAsString)
+		fmt.Println(ts.UTC())
+	}
+}
